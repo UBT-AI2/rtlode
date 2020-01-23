55dbcb3eb7a62e47c84b271f0eede2e8f5eaa147
Silas Bartel <silas.a.bartel@gmail.com>
2020-01-23 11:05:57 +0100

    Extracted generic reduce method. Added reduce_and.

--- vector_utils.py
+++ vector_utils.py
@@ -1,6 +1,7 @@
 from myhdl import block, Signal, instances, always_comb, always_seq
 
 import num
+from utils import clone_signal
 
 
 class UnequalVectorLength(Exception):
@@ -8,37 +9,38 @@ class UnequalVectorLength(Exception):
 
 
 @block
-def reduce_sum(in_vector, out_sum, clk=None):
+def reduce(in_vector, op, out, clk=None):
     """
-    Computes the sum of all in_vector elements.
+    Reduces vector with given operator.
     If clk is provided the propagation delay is 1 clk cycle.
-    Only the output is latched, the internal adder are still
+    Only the output is latched, the internal operators are still
     working combinatorical.
 
     :param in_vector: vector of elements
-    :param out_sum: sum off all vector elements
+    :param op: the operation to be applied to each value pair
+    :param out: result of reduce operation
     :param clk: optional clk for output assignment
     :return: myhdl instances
     """
     n_elements = len(in_vector)
 
-    partial_sums = [Signal(num.same_as(out_sum)) for i in range(max(n_elements - 1, 0))]
-    add_insts = [None for i in range(max(n_elements - 1, 0))]
+    partial_results = [clone_signal(out) for i in range(max(n_elements - 1, 0))]
+    op_insts = [None for i in range(max(n_elements - 1, 0))]
 
-    result = Signal(num.same_as(out_sum, val=0))
+    result = Signal(num.same_as(out, val=0))
     if n_elements == 1:
         result = in_vector[0]
     elif n_elements >= 2:
-        result = partial_sums[n_elements - 2]
+        result = partial_results[n_elements - 2]
 
     for i in range(max(n_elements - 1, 0)):
         if i == 0:
-            add_insts[i] = num.add(in_vector[i], in_vector[i + 1], partial_sums[i])
+            op_insts[i] = op(in_vector[i], in_vector[i + 1], partial_results[i])
         else:
-            add_insts[i] = num.add(partial_sums[i - 1], in_vector[i + 1], partial_sums[i])
+            op_insts[i] = op(partial_results[i - 1], in_vector[i + 1], partial_results[i])
 
     def assign_out():
-        out_sum.next = result
+        out.next = result
 
     if clk is not None:
         calc = always_seq(clk.posedge, reset=None)(assign_out)
@@ -46,6 +48,45 @@ def reduce_sum(in_vector, out_sum, clk=None):
         calc = always_comb(assign_out)
 
     return instances()
+
+
+@block
+def reduce_and(in_vector, res, clk=None):
+    """
+    Checks if all elements of the vector are true.
+    If clk is provided the propagation delay is 1 clk cycle.
+    Only the output is latched, the internal and logic is still
+    working combinatorical.
+
+    :param in_vector: vector of elements
+    :param res: sum off all vector elements
+    :param clk: optional clk for output assignment
+    :return: myhdl instances
+    """
+    @block
+    def logical_and(a, b, c):
+        @always_comb
+        def assign():
+            c.next = a and b
+        return instances()
+
+    return reduce(in_vector, logical_and, res, clk=clk)
+
+
+@block
+def reduce_sum(in_vector, res, clk=None):
+    """
+    Computes the sum of all in_vector elements.
+    If clk is provided the propagation delay is 1 clk cycle.
+    Only the output is latched, the internal adder are still
+    working combinatorical.
+
+    :param in_vector: vector of elements
+    :param res: sum off all vector elements
+    :param clk: optional clk for output assignment
+    :return: myhdl instances
+    """
+    return reduce(in_vector, num.add, res, clk=clk)
 
 
 @block
