8f4f30d4162d198984de0c99bf7105bc0e3523fc
Silas Bartel <silas.a.bartel@gmail.com>
2020-04-14 17:46:57 +0200

    Fixed missing clk and wrong reset handling for solvers.

--- generator/dispatcher.py
+++ generator/dispatcher.py
@@ -1,6 +1,7 @@
 from myhdl import block, always_seq, enum, Signal, instances, always_comb
 
 from generator.cdc_utils import FifoConsumer, FifoProducer
+from generator.flow import FlowControl
 from generator.hram import get_input_desc, get_output_desc
 from generator.runge_kutta import SolverInterface, rk_solver
 from utils import num
@@ -30,38 +31,45 @@ def dispatcher(config, data_in: FifoConsumer, data_out: FifoProducer):
     solver_output_packed = solver_output.packed()
 
     current_data_id = Signal(num.integer())
-    solver = SolverInterface(config.system_size)
+    solver = SolverInterface(config.system_size, flow=FlowControl(clk=clk))
     solver_inst = rk_solver(config, solver)
 
-    @always_seq(clk.posedge, reset=rst)
+    @always_seq(clk.posedge, reset=None)
     def state_machine():
-        if state == t_state.IDLE:
+        if rst:
             data_out.wr.next = False
-            solver.flow.rst.next = False
-
-            solver.h.next = solver_input.h
-            solver.n.next = solver_input.n
-            solver.x_start.next = solver_input.x_start
-            for i in range(config.system_size):
-                solver.y_start[i].next = solver_input.y_start[i]
-            current_data_id.next = solver_output.id
-
-            if not data_in.empty:
-                data_in.rd.next = True
-                solver.flow.enb.next = True
-                state.next = t_state.BUSY
-        elif state == t_state.BUSY:
             data_in.rd.next = False
+            solver.flow.enb.next = False
+            solver.flow.rst.next = True
+            state.next = t_state.IDLE
+        else:
+            if state == t_state.IDLE:
+                data_out.wr.next = False
+                solver.flow.rst.next = False
 
-            solver_output.x.next = solver.x
-            for i in range(config.system_size):
-                solver_output.y[i].next = solver.y[i]
-            solver_output.id.next = current_data_id
+                solver.h.next = solver_input.h
+                solver.n.next = solver_input.n
+                solver.x_start.next = solver_input.x_start
+                for i in range(config.system_size):
+                    solver.y_start[i].next = solver_input.y_start[i]
+                current_data_id.next = solver_output.id
 
-            if solver.flow.fin:
-                data_out.wr.next = True
-                solver.flow.rst.next = True
-                state.next = t_state.IDLE
+                if not data_in.empty:
+                    data_in.rd.next = True
+                    solver.flow.enb.next = True
+                    state.next = t_state.BUSY
+            elif state == t_state.BUSY:
+                data_in.rd.next = False
+
+                solver_output.x.next = solver.x
+                for i in range(config.system_size):
+                    solver_output.y[i].next = solver.y[i]
+                solver_output.id.next = current_data_id
+
+                if solver.flow.fin:
+                    data_out.wr.next = True
+                    solver.flow.rst.next = True
+                    state.next = t_state.IDLE
 
     @always_comb
     def assign_solver_output():
