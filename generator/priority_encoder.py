8fc5606120ca94052f77e797833750841adf82f2
Silas Bartel <silas.a.bartel@gmail.com>
2020-04-29 10:49:30 +0200

    Implemented one hot priority encoder.

---
+++ generator/priority_encoder.py
@@ -0,0 +1,54 @@
+from myhdl import instances, block, SignalType, always_comb, Signal, ConcatSignal
+
+
+@block
+def highest_bit_checker(
+        in_vec: SignalType,
+        out_bit: SignalType,
+        index
+):
+    @always_comb
+    def check():
+        if in_vec[index] and in_vec[:index + 1] == 0:
+            out_bit.next = True
+        else:
+            out_bit.next = False
+
+    return instances()
+
+
+@block
+def priority_encoder_one_hot(
+        in_vec: SignalType,
+        out_vec: SignalType
+):
+    """
+    Highest order bit gets priority and outputed in out_vec.
+    :param in_vec: input vector
+    :param out_vec: out vector one hot
+    :return: myhdl instances
+    """
+    assert len(in_vec) == len(out_vec)
+
+    bit_width = len(in_vec)
+    priority_bits = [Signal(bool(0)) for _ in range(bit_width)]
+    priority_vec = ConcatSignal(*reversed(priority_bits))
+
+    highest_bit = priority_bits[bit_width - 1]
+
+    @always_comb
+    def highest_index():
+        if in_vec[bit_width - 1]:
+            highest_bit.next = True
+        else:
+            highest_bit.next = False
+
+    other_indeces = [
+        highest_bit_checker(in_vec, priority_bits[i], i) for i in range(bit_width - 1)
+    ]
+
+    @always_comb
+    def assign_out():
+        out_vec.next = priority_vec
+
+    return instances()
---
+++ generator/tests/test_priority_encoder.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from myhdl import block, delay, Signal, intbv, instance, instances, StopSimulation
+
+from generator.priority_encoder import priority_encoder_one_hot
+
+
+class Test(TestCase):
+    datawidth = 8
+    dataset = {
+        0b10101010: 0b10000000,
+        0b00000000: 0b00000000,
+        0b00101010: 0b00100000,
+        0b00000001: 0b00000001,
+    }
+
+    def test_priority_encoder_one_hot(self):
+        @block
+        def testbench():
+            in_val = Signal(intbv(0)[self.datawidth:])
+            out_val = Signal(intbv(0)[self.datawidth:])
+
+            dut = priority_encoder_one_hot(in_val, out_val)
+
+            @instance
+            def input_driver():
+                for in_data, out_data in self.dataset.items():
+                    in_val.next = in_data
+                    yield delay(10)
+                    self.assertEqual(out_data, out_val)
+                raise StopSimulation()
+
+            return instances()
+
+        tb = testbench()
+        tb.config_sim(trace=True)
+        tb.run_sim()
