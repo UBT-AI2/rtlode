af6be5110cc0abe65cd8ecab4d337a7c3fca05d6
Silas Bartel <silas.a.bartel@gmail.com>
2020-04-29 17:57:27 +0200

    Reworked system test as new sim routine.

--- generator/generator.py
+++ generator/generator.py
@@ -3,15 +3,19 @@ import os
 import shutil
 import subprocess
 import uuid
+from typing import List
 
 import yaml
-from myhdl import Signal, ResetSignal
+from myhdl import Signal, ResetSignal, instance, delay, always, always_seq, instances, block, StopSimulation
 
+from common import data_desc
 from generator.ccip import CcipRx, CcipTx
 from common.config import Config
 from generator.afu import afu
+from generator.cdc_utils import FifoProducer, FifoConsumer, async_fifo
 from generator.csr import csr_addresses
 from common.packed_struct import BitVector
+from generator.dispatcher import dispatcher
 from utils import slv, num
 
 
@@ -72,8 +76,117 @@ def _create_build_config(config):
         json.dump(_get_build_config(config), f, ensure_ascii=False, indent=2)
 
 
-def simulate(*config_files):
-    pass  # TODO reimplement
+def simulate(*config_files, trace=False, buffer_size_bits=4, runtime_config=None, nbr_datasets=10):
+    current_input_id = 0
+
+    def create_input_data(x_start: float, y_start: List[float], h: int, n: int):
+        nonlocal current_input_id
+        system_size = len(y_start)
+
+        input_desc = data_desc.get_input_desc(system_size)
+        input_data = input_desc.create_write_instance()
+
+        input_data.x_start.next = num.signal_from_float(x_start)
+        for i in range(system_size):
+            input_data.y_start[i].next = num.signal_from_float(y_start[i])
+        input_data.h.next = num.signal_from_float(h)
+        input_data.n.next = int(n)
+
+        current_input_id = current_input_id + 1
+        input_data.id.next = int(current_input_id)
+
+        input_data.update()
+        input_packed = input_data.packed()
+        return int(input_packed)
+
+    @block
+    def testbench():
+        config_dict = _load_config(*config_files)
+        if runtime_config is not None:
+            config_dict.update(runtime_config)
+        config = Config.from_dict(config_dict)
+
+        clk = Signal(bool(0))
+        reset = ResetSignal(True, True, False)
+        usr_clk = Signal(bool(0))
+        usr_reset = ResetSignal(True, True, False)
+
+        input_desc_vec = BitVector(len(data_desc.get_input_desc(config.system_size)))
+        output_desc_vec = BitVector(len(data_desc.get_output_desc(config.system_size)))
+
+        in_fifo_p = FifoProducer(clk, reset, input_desc_vec.create_instance())
+        in_fifo_c = FifoConsumer(usr_clk, usr_reset, input_desc_vec.create_instance())
+        in_fifo = async_fifo(in_fifo_p, in_fifo_c, buffer_size_bits=buffer_size_bits)
+
+        out_fifo_p = FifoProducer(usr_clk, usr_reset, output_desc_vec.create_instance())
+        out_fifo_c = FifoConsumer(clk, reset, output_desc_vec.create_instance())
+        out_fifo = async_fifo(out_fifo_p, out_fifo_c, buffer_size_bits=buffer_size_bits)
+
+        disp_inst = dispatcher(config, data_in=in_fifo_c, data_out=out_fifo_p)
+
+        parsed_output_data = data_desc.get_output_desc(config.system_size).create_read_instance(out_fifo_c.data)
+
+        in_fifo_p.data.next = create_input_data(
+            config_dict['x'],
+            config_dict['y'],
+            config_dict['h'],
+            config_dict['n']
+        )
+        in_fifo_p.data._update()
+
+        @instance
+        def reset_handler():
+            yield delay(200)
+            reset.next = False
+            usr_reset.next = False
+
+        @always(delay(10))
+        def clk_driver():
+            clk.next = not clk
+
+        @always(delay(43))
+        def usr_clk_driver():
+            usr_clk.next = not usr_clk
+
+        @instance
+        def p_write():
+            while True:
+                yield clk.posedge
+                in_fifo_p.wr.next = True
+                if in_fifo_p.wr and not in_fifo_p.full:
+                    in_fifo_p.data.next = create_input_data(
+                        config_dict['x'],
+                        config_dict['y'],
+                        config_dict['h'],
+                        config_dict['n']
+                    )
+
+        clks = 0
+
+        @always_seq(out_fifo_c.clk.posedge, reset=None)
+        def c_read():
+            nonlocal clks
+            clks = clks + 1
+            out_fifo_c.rd.next = True
+            if out_fifo_c.rd and not out_fifo_c.empty:
+                y = []
+                for el in parsed_output_data.y:
+                    y.append(num.to_float(el))
+
+                print("Out: %r" % {
+                    'x': num.to_float(parsed_output_data.x),
+                    'y': y,
+                    'id': parsed_output_data.id
+                })
+                if parsed_output_data.id == nbr_datasets:
+                    print("Finished after %i clock cycles." % clks)
+                    raise StopSimulation()
+
+        return instances()
+
+    tb = testbench()
+    tb.config_sim(trace=trace)
+    tb.run_sim()
 
 
 def convert(config):
