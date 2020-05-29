65cd5819d9ddcf90cbfaa0844085931d394fd2a3
Silas Bartel <silas.a.bartel@gmail.com>
2020-05-29 14:14:37 +0200

    Improved lincomb performance by dropping elements with a 0 factor.

--- generator/vector_utils/lincomb.py
+++ generator/vector_utils/lincomb.py
@@ -1,9 +1,9 @@
-from myhdl import block, Signal
+from myhdl import block, Signal, SignalType
 
 import generator.calc
 from generator.flow import FlowControl
 from generator.pipeline import Pipeline
-from generator.utils import bind
+from generator.utils import bind, assign_flow
 from generator.vector_utils.reduce import reduce_sum
 from utils import num
 
@@ -26,12 +26,25 @@ def lincomb(in_a, in_b, out_sum):
         raise UnequalVectorLength("len(in_a) = %d != len(in_b) = %d" % (len(in_a), len(in_b)))
     n_elements = len(in_a)
 
-    if n_elements == 1:
+    # Remove elements where one factor is 0
+    valid = [
+        (isinstance(in_a[i], SignalType) or in_a[i] != 0)
+        and (isinstance(in_b[i], SignalType) or in_b[i] != 0)
+        for i in range(n_elements)
+    ]
+    in_a = [in_a[i] for i in range(n_elements) if valid[i]]
+    in_b = [in_b[i] for i in range(n_elements) if valid[i]]
+    n_elements = len(in_a)
+
+    pipe = Pipeline()
+    if n_elements == 0:
+        pipe.then(bind(assign_flow, 0, out_sum))
+        return pipe
+    elif n_elements == 1:
         partial_results = [out_sum]
     else:
         partial_results = [Signal(num.same_as(out_sum)) for _ in range(n_elements)]
 
-    pipe = Pipeline()
     pipe.then([bind(generator.calc.mul, in_a[i], in_b[i], partial_results[i]) for i in range(n_elements)])
     if n_elements > 1:
         pipe.then(bind(reduce_sum, partial_results, out_sum))
