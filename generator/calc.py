75e2843389bd7433793ca1e922eda0d173f35ce2
Silas Bartel <silas.a.bartel@gmail.com>
2020-05-29 14:42:14 +0200

    Multiplications will now be implemented in shifts whenever possible.

--- generator/calc.py
+++ generator/calc.py
@@ -3,6 +3,7 @@ from typing import Union
 from myhdl import block, always_seq, always_comb, Signal, intbv, SignalType
 
 from generator.flow import FlowControl
+from generator.utils import assign_flow
 from utils.num import DOUBLE_MIN_VALUE, DOUBLE_MAX_VALUE, NONFRACTION_SIZE, FRACTION_SIZE
 
 
@@ -37,35 +38,47 @@ def sub(in_a, in_b, out_c, flow: FlowControl = None):
 
 
 @block
-def mul(in_a: Union[int, SignalType], in_b: SignalType, out_c, flow: FlowControl = None):
-    enable = flow.enb if flow is not None else True
-
+def mul(in_a: Union[int, SignalType], in_b: SignalType, out_c, flow: FlowControl):
     reg = Signal(intbv(0, min=DOUBLE_MIN_VALUE, max=DOUBLE_MAX_VALUE))
+
+    if isinstance(in_a, int) and bin(in_a).count('1') == 1:
+        # This multiplication can be implemented in shifts.
+        bin_repr = bin(in_a)
+        shift_by = len(bin_repr) - 1 - bin_repr.index('1') - FRACTION_SIZE
+        print('Implemented multiplication as shift by: ', shift_by)
+        if shift_by == 0:
+            return assign_flow(in_b, out_c, flow)
+        elif shift_by > 0:
+            @always_comb
+            def shift_left():
+                if flow.enb:
+                    out_c.next = in_b << shift_by
+                flow.fin.next = flow.enb
+            return shift_left
+        elif shift_by < 0:
+            shift_by = -shift_by
+
+            @always_comb
+            def shift_right():
+                if flow.enb:
+                    out_c.next = in_b >> shift_by
+                flow.fin.next = flow.enb
+            return shift_right
 
     @always_comb
     def resize():
         out_c.next = reg[NONFRACTION_SIZE + FRACTION_SIZE + 1 + FRACTION_SIZE:FRACTION_SIZE].signed()
 
-    if flow is not None:
-        if isinstance(in_a, int):
-            @always_seq(flow.clk_edge(), flow.rst)
-            def calc():
-                if flow.enb:
-                    reg.next = (intbv(in_a).signed() * in_b)
-                    flow.fin.next = True
-        else:
-            @always_seq(flow.clk_edge(), flow.rst)
-            def calc():
-                if flow.enb:
-                    reg.next = (in_a * in_b)
-                    flow.fin.next = True
-    else:
-        if isinstance(in_a, int):
-            @always_comb
-            def calc():
+    if isinstance(in_a, int):
+        @always_seq(flow.clk_edge(), flow.rst)
+        def calc():
+            if flow.enb:
                 reg.next = (intbv(in_a).signed() * in_b)
-        else:
-            @always_comb
-            def calc():
+                flow.fin.next = True
+    else:
+        @always_seq(flow.clk_edge(), flow.rst)
+        def calc():
+            if flow.enb:
                 reg.next = (in_a * in_b)
+                flow.fin.next = True
     return [resize, calc]
