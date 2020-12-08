1f5688157a60abb710e2ab3ad25b7de6e708e5ed
Silas Bartel <silas.a.bartel@gmail.com>
2020-12-08 21:46:51 +0100

    Added basic implementation for floating point pipeline_elements. Currently only double precision supported. Tests included.

--- framework/pipeline_elements_floating.py
+++ framework/pipeline_elements_floating.py
@@ -1,3 +1,109 @@
+from myhdl import block, Signal, instances, always_seq, always_comb
+
+from framework.pipeline import PipelineNode, PipeConstant
+from framework.fifo import FifoProducer, FifoConsumer, fifo
+from utils import num
+
+
+@block
+def register(clk, rst, previous_signal, next_signal):
+
+    @always_seq(clk.posedge, reset=rst)
+    def logic():
+        next_signal.next = previous_signal
+
+    return instances()
+
+
+@block
+def mul_wrapper(clk, rst, in_stage_valid, in_stage_busy, out_stage, out_busy, node_input, node_output, inner_pipeline=None, inner_latency=5):
+    num_factory = num.get_numeric_factory()
+
+    inner_pipe_res = Signal(num_factory.create())
+    inner_pipe = inner_pipeline(clk, node_input.a, node_input.b, inner_pipe_res, pipeline_latency=inner_latency)
+
+    in_valid = Signal(num.get_bool_factory().create())
+
+    @always_seq(clk.posedge, reset=rst)
+    def check_input_validity():
+        in_valid.next = in_stage_valid and not in_stage_busy
+
+    valid_signals = [Signal(num.get_bool_factory().create()) for _ in range(inner_latency - 1)]
+    valid_reg = []
+
+    previous_signal = in_valid
+    for next_signal in valid_signals:
+        valid_reg.append(
+            register(clk, rst, previous_signal, next_signal)
+        )
+        previous_signal = next_signal
+    valid = previous_signal
+
+    # Must be a fifo of size inner_latency
+    reg_fifo_p = FifoProducer(Signal(num_factory.create()))
+    reg_fifo_c = FifoConsumer(Signal(num_factory.create()))
+    reg_fifo = fifo(clk, rst, reg_fifo_p, reg_fifo_c, buffer_size_bits=4)  # TODO
+
+    reg_fill_level = Signal(num.get_integer_factory().create(0))
+
+    @always_seq(clk.posedge, reset=rst)
+    def drive_data():
+        reg_fifo_p.wr.next = False
+        if not out_busy:
+            if reg_fifo_c.empty:
+                node_output.default.next = inner_pipe_res
+            else:
+                node_output.default.next = reg_fifo_c.data
+                reg_fill_level.next = reg_fill_level - 1
+
+        if valid and ((out_stage.valid and out_busy) or not reg_fifo_c.empty):
+            reg_fifo_p.data.next = inner_pipe_res
+            reg_fifo_p.wr.next = True
+            reg_fill_level.next = reg_fill_level + 1
+
+    @always_comb
+    def drive_fifo_c():
+        reg_fifo_c.rd.next = not out_busy
+
+    return instances()
+
+
+@block
+def mul_altfp_double(clk, dataa, datab, result, pipeline_latency=5):
+    num_factory = num.get_numeric_factory()
+
+    internal_pipeline = [0 for _ in range(pipeline_latency - 1)]
+
+    @always_seq(clk.posedge, reset=None)
+    def logic():
+        internal_pipeline.insert(
+            0,
+            num_factory.create_constant(
+                num_factory.value_of(dataa) * num_factory.value_of(datab)
+            )
+        )
+        result.next = internal_pipeline.pop()
+
+    result.driven = 'reg'
+
+    return instances()
+
+
+mul_altfp_double.verilog_code = \
+    """
+altfp_mult	altfp_mult_component (
+                .clock ($clk),
+                .dataa ($dataa),
+                .datab ($datab),
+                .result ($result));
+    defparam
+        altfp_mult_component.denormal_support = "NO",
+        altfp_mult_component.lpm_type = "altfp_mult",
+        altfp_mult_component.reduced_functionality = "NO",
+        altfp_mult_component.pipeline = $pipeline_latency,
+        altfp_mult_component.width_exp = 11,
+        altfp_mult_component.width_man = 52;
+    """
 
 
 def mul(a, b):
@@ -10,7 +116,73 @@ def mul(a, b):
     :param b: parameter b
     :return: int or pipeline node
     """
-    raise NotImplementedError
+    num_factory = num.get_numeric_factory()
+    if isinstance(a, PipeConstant) and isinstance(b, PipeConstant):
+        return PipeConstant.from_float(
+            num_factory.value_of(a.get_value()) * num_factory.value_of(b.get_value())
+        )
+    elif isinstance(a, PipeConstant) or isinstance(b, PipeConstant):
+        if isinstance(a, PipeConstant):
+            static_value = a.get_value()
+            dynamic_value = b
+        else:
+            static_value = b.get_value()
+            dynamic_value = a
+
+        if static_value == 0:
+            return PipeConstant.from_float(0)
+        elif static_value == 1:
+            return dynamic_value
+
+    node = PipelineNode(6)
+
+    node.add_inputs(a=a, b=b)
+    res = Signal(num_factory.create())
+    node.add_output(res)
+    node.set_name('mul')
+
+    node.set_logic(mul_wrapper, inner_latency=5, inner_pipeline=mul_altfp_double)
+    return node
+
+
+@block
+def add_altfp_double(clk, dataa, datab, result, pipeline_latency=5):
+    num_factory = num.get_numeric_factory()
+
+    internal_pipeline = [0 for _ in range(pipeline_latency - 1)]
+
+    @always_seq(clk.posedge, reset=None)
+    def logic():
+        internal_pipeline.insert(
+            0,
+            num_factory.create_constant(
+                num_factory.value_of(dataa) + num_factory.value_of(datab)
+            )
+        )
+        result.next = internal_pipeline.pop()
+
+    result.driven = 'reg'
+
+    return instances()
+
+
+add_altfp_double.verilog_code = \
+    """
+altfp_add_sub	altfp_add_sub_component (
+                .clock ($clk),
+                .dataa ($dataa),
+                .datab ($datab),
+                .result ($result));
+    defparam
+        altfp_mult_component.denormal_support = "NO",
+        altfp_mult_component.lpm_type = "altfp_add_sub",
+        altfp_mult_component.reduced_functionality = "NO",
+        altfp_mult_component.direction = "ADD",
+        altfp_mult_component.rounding = "TO_NEAREST",
+        altfp_mult_component.pipeline = $pipeline_latency,
+        altfp_mult_component.width_exp = 11,
+        altfp_mult_component.width_man = 52;
+    """
 
 
 def add(a, b):
@@ -23,7 +195,73 @@ def add(a, b):
     :param b: parameter b
     :return: int or pipeline node
     """
-    raise NotImplementedError
+    inner_latency = 7
+
+    num_factory = num.get_numeric_factory()
+    if isinstance(a, PipeConstant) and isinstance(b, PipeConstant):
+        return PipeConstant.from_float(
+            num_factory.value_of(a.get_value()) + num_factory.value_of(b.get_value())
+        )
+    elif isinstance(a, PipeConstant) or isinstance(b, PipeConstant):
+        if isinstance(a, PipeConstant):
+            static_value = a.get_value()
+            dynamic_value = b
+        else:
+            static_value = b.get_value()
+            dynamic_value = a
+
+        if static_value == 0:
+            return dynamic_value
+
+    node = PipelineNode(inner_latency + 1)
+
+    node.add_inputs(a=a, b=b)
+    res = Signal(num_factory.create())
+    node.add_output(res)
+    node.set_name('mul')
+
+    node.set_logic(mul_wrapper, inner_latency=inner_latency, inner_pipeline=add_altfp_double)
+    return node
+
+
+@block
+def sub_altfp_double(clk, dataa, datab, result, pipeline_latency=5):
+    num_factory = num.get_numeric_factory()
+
+    internal_pipeline = [0 for _ in range(pipeline_latency - 1)]
+
+    @always_seq(clk.posedge, reset=None)
+    def logic():
+        internal_pipeline.insert(
+            0,
+            num_factory.create_constant(
+                num_factory.value_of(dataa) - num_factory.value_of(datab)
+            )
+        )
+        result.next = internal_pipeline.pop()
+
+    result.driven = 'reg'
+
+    return instances()
+
+
+sub_altfp_double.verilog_code = \
+    """
+altfp_add_sub	altfp_add_sub_component (
+                .clock ($clk),
+                .dataa ($dataa),
+                .datab ($datab),
+                .result ($result));
+    defparam
+        altfp_mult_component.denormal_support = "NO",
+        altfp_mult_component.lpm_type = "altfp_add_sub",
+        altfp_mult_component.reduced_functionality = "NO",
+        altfp_mult_component.direction = "SUB",
+        altfp_mult_component.rounding = "TO_NEAREST",
+        altfp_mult_component.pipeline = $pipeline_latency,
+        altfp_mult_component.width_exp = 11,
+        altfp_mult_component.width_man = 52;
+    """
 
 
 def sub(a, b):
@@ -36,7 +274,33 @@ def sub(a, b):
     :param b: parameter b
     :return: int or pipeline node
     """
-    raise NotImplementedError
+    inner_latency = 7
+
+    num_factory = num.get_numeric_factory()
+    if isinstance(a, PipeConstant) and isinstance(b, PipeConstant):
+        return PipeConstant.from_float(
+            num_factory.value_of(a.get_value()) - num_factory.value_of(b.get_value())
+        )
+    elif isinstance(a, PipeConstant) or isinstance(b, PipeConstant):
+        if isinstance(a, PipeConstant):
+            static_value = a.get_value()
+            dynamic_value = b
+        else:
+            static_value = b.get_value()
+            dynamic_value = a
+
+        if static_value == 0:
+            return dynamic_value
+
+    node = PipelineNode(inner_latency + 1)
+
+    node.add_inputs(a=a, b=b)
+    res = Signal(num_factory.create())
+    node.add_output(res)
+    node.set_name('mul')
+
+    node.set_logic(mul_wrapper, inner_latency=inner_latency, inner_pipeline=sub_altfp_double)
+    return node
 
 
 def negate(val):
@@ -47,4 +311,4 @@ def negate(val):
     :param val: parameter val
     :return: int or pipeline node
     """
-    raise NotImplementedError
+    return val * PipeConstant.from_float(-1.0)
---
+++ framework/tests/test_pipeline_elements_floating.py
@@ -0,0 +1,117 @@
+from framework.pipeline import PipeConstant
+from framework.pipeline_elements import add, mul, negate
+from framework.tests.helper import PipeTestCase
+from utils import num
+
+
+class TestPipe(PipeTestCase):
+    def run(self, result=None):
+        # Run twice for each floating precission
+        num.set_numeric_factory(num.FloatingNumberFactory(num.FloatingPrecission.DOUBLE))
+        super().run(result)
+        # TODO add additional run with single
+        # num.set_numeric_factory(num.FloatingNumberFactory(num.FloatingPrecission.SINGLE))
+        # super().run(result)
+
+    def test_add(self):
+        """
+        Testing addition.
+        """
+        def inner_pipe(data):
+            res = data + PipeConstant.from_float(5)
+            return res
+
+        self.run_pipe(inner_pipe, list(range(40)), [i + 5 for i in range(40)])
+
+    def test_add_reverse(self):
+        """
+        Testing addition reverse operator.
+        """
+        def inner_pipe(data):
+            res = PipeConstant.from_float(5) + data
+            return res
+
+        self.run_pipe(inner_pipe, list(range(40)), [5 + i for i in range(40)])
+
+    def test_add_only_constants(self):
+        """
+        Testing multiplication with two constants.
+        """
+        def inner_pipe(data):
+            add1 = add(PipeConstant.from_float(2), PipeConstant.from_float(3))
+            add2 = add(data, add1)
+            return add2
+
+        self.run_pipe(inner_pipe, list(range(40)), [i + 5 for i in range(40)])
+
+    def test_sub(self):
+        """
+        Testing substraction.
+        """
+        def inner_pipe(data):
+            res = data - PipeConstant.from_float(5)
+            return res
+
+        self.run_pipe(inner_pipe, list(range(40)), [i - 5 for i in range(40)])
+
+    def test_sub_reverse(self):
+        """
+        Testing substraction reverse operator.
+        """
+        def inner_pipe(data):
+            res = PipeConstant.from_float(5) - data
+            return res
+
+        self.run_pipe(inner_pipe, list(range(40)), [5 - i for i in range(40)])
+
+    def test_negate(self):
+        """
+        Testing substraction.
+        """
+        def inner_pipe(data):
+            res = negate(data)
+            return res
+
+        self.run_pipe(inner_pipe, list(range(1, 40)), [-i for i in range(1, 40)])
+
+    def test_mul_only_constants(self):
+        """
+        Testing multiplication with two constants.
+        """
+        def inner_pipe(data):
+            mul1 = mul(PipeConstant.from_float(2), PipeConstant.from_float(3))
+            add1 = add(data, mul1)
+            return add1
+
+        self.run_pipe(inner_pipe, list(range(40)), [i + 6 for i in range(40)])
+
+    def test_mul_by_constant(self):
+        """
+        Testing multiplication with constant factor.
+        """
+        def inner_pipe(data):
+            mul1 = mul(data, PipeConstant.from_float(3))
+            add1 = add(mul1, PipeConstant.from_float(3))
+            return add1
+
+        self.run_pipe(inner_pipe, list(range(40)), [(i * 3) + 3 for i in range(40)])
+
+    def test_mul(self):
+        """
+        Testing multiplication operator.
+        """
+        def inner_pipe(data):
+            res = data * PipeConstant.from_float(5)
+            return res
+
+        self.run_pipe(inner_pipe, list(range(40)), [i * 5 for i in range(40)])
+
+    def test_mul_reverse(self):
+        """
+        Testing mutiplication reverse operator.
+        """
+        def inner_pipe(data):
+            res = PipeConstant.from_float(5) * data
+            return res
+
+        self.run_pipe(inner_pipe, list(range(40)), [5 * i for i in range(40)])
